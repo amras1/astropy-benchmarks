8a94a40b635e0915a99dbeade805f4976f6c7aff
Michael Mueller <michaeldmueller7@gmail.com>
2014-06-10 18:34:20 -0400

    Oops, forgot to include version of comparison benchmarks with writing earlier

--- benchmarks/comparisonbench.py
+++ benchmarks/comparisonbench.py
@@ -8,10 +8,17 @@ class ComparisonSuite:
         self.mix_dtype = np.dtype({'names': '123456789', 'formats':
                 ('i4', 'f4', 'S10', 'i4', 'f4', 'S10', 'i4', 'f4', 'S10')})
         self.data = {}
+        self.tables = {}
+        self.frames = {}
         for datatype in ('float', 'int', 'string', 'mixture'):
             f = open('benchmarks/files/comparison/{}.txt'.format(datatype))
             self.data[datatype] = f.read()
             f.close()
+            self.tables[datatype] = ascii.read(StringIO(self.data[datatype]),
+                                               format='basic')
+            self.frames[datatype] = pandas.read_csv(StringIO(
+                                    self.data[datatype]), sep=' ')
+        self.out = StringIO()
                                   
     def time_astropy_float(self):
         ascii.read(StringIO(self.data['float']), format='basic')
@@ -24,6 +31,18 @@ class ComparisonSuite:
 
     def time_astropy_mixture(self):
         ascii.read(StringIO(self.data['mixture']), format='basic')
+
+    def time_astropy_write_float(self):
+        ascii.write(self.tables['float'], self.out, Writer=ascii.Basic)
+                                  
+    def time_astropy_write_int(self):
+        ascii.write(self.tables['int'], self.out, Writer=ascii.Basic)
+                                  
+    def time_astropy_write_string(self):
+        ascii.write(self.tables['string'], self.out, Writer=ascii.Basic)
+
+    def time_astropy_write_mixture(self):
+        ascii.write(self.tables['mixture'], self.out, Writer=ascii.Basic)
 
     def time_numpy_loadtxt_float(self):
         np.loadtxt(StringIO(self.data['float']), delimiter=' ')
@@ -56,6 +75,19 @@ class ComparisonSuite:
         np.genfromtxt(StringIO(self.data['mixture']), delimiter=' ',
                       dtype=None)
 
+    def time_numpy_savetxt_float(self):
+        np.savetxt(self.out, self.tables['float'])
+
+    def time_numpy_savetxt_int(self):
+        np.savetxt(self.out, self.tables['int'], fmt='%i')
+
+    def time_numpy_savetxt_string(self):
+        np.savetxt(self.out, self.tables['string'], fmt='%s')
+
+    def time_numpy_savetxt_mixture(self):
+        np.savetxt(self.out, self.tables['mixture'], fmt='%i %f %s ' \
+                   '%i %f %s %i %f %s')
+
     def time_pandas_read_table_float(self):
         pandas.read_table(StringIO(self.data['float']), sep=' ')
 
@@ -79,3 +111,15 @@ class ComparisonSuite:
 
     def time_pandas_read_csv_mixture(self):
         pandas.read_csv(StringIO(self.data['mixture']), sep=' ')
+
+    def time_pandas_write_float(self):
+        self.frames['float'].to_csv(self.out, sep=' ')
+
+    def time_pandas_write_int(self):
+        self.frames['int'].to_csv(self.out, sep=' ')
+
+    def time_pandas_write_string(self):
+        self.frames['string'].to_csv(self.out, sep=' ')
+
+    def time_pandas_write_mixture(self):
+        self.frames['mixture'].to_csv(self.out, sep=' ')
